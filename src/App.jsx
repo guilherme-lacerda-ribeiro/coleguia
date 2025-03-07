d6e8a2e8277694326a22c74a9e06fd1ef33858e7
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-07 19:17:04 -0300

    layout spaced vertically

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,20 @@ function App() {
   const [darkMode, setDarkMode] = useState(true)
 
   return (
-    <div id='main-wrapper' className={`${darkMode ? 'dark' : ''} w-screen h-screen p-4 bg-white dark:bg-black text-black dark:text-slate-400`}>
-      <header className='px-7 py-4 border-b border-slate-600'>
-        <Header>
-          <Menu />
-        </Header>
-      </header>
-      <main>
-        <section>
+    <div id='outter' className={`${darkMode ? 'dark' : ''} w-screen h-screen p-4 bg-white dark:bg-black text-black dark:text-slate-400`}>
+      <div id='inner' className='h-full w-full flex flex-col justify-between border-1 rounded-lg border-slate-600'>
+        <header className='px-7 py-4 border-b border-slate-600'>
+          <Header>
+            <Menu />
+          </Header>
+        </header>
+        <main>
           <DarkMode setDarkMode={setDarkMode} />
-        </section>
-      </main>
-      <footer className='border-t border-slate-600'>
-        <Footer />
-      </footer>
+        </main>
+        <footer className='border-t border-slate-600'>
+          <Footer />
+        </footer>
+      </div>
     </div>
   )
 }
