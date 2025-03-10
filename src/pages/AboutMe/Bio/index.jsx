dc90186714e127dbd0797dd14695428074be2f8a
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 15:54:55 -0300

    personal info finished

--- src/pages/AboutMe/Bio/index.jsx
+++ src/pages/AboutMe/Bio/index.jsx
@@ -1,8 +1,8 @@
 const Bio = () => {
   return (
     <>
-      <p>📌 Born in 1984, in <a className="underline text-sky-600" href="https://maps.app.goo.gl/VAUYDJj3nDGwifr3A" target='_blank'>Belo Horizonte, MG, Brazil</a>.</p>
-      <p>👶 Spent my childhood as an active child, engaging in sports, video games, and developing strong communication skills.</p>
+      <p className="mb-3">📌 Born in 1984, in <a className="underline text-sky-600" href="https://maps.app.goo.gl/VAUYDJj3nDGwifr3A" target='_blank'>Belo Horizonte, MG, Brazil</a>.</p>
+      <p className="mb-3">👶 Spent my childhood as an active child, engaging in sports, video games, and developing strong communication skills.</p>
       <p>👨‍👩‍👧‍👦 I was raised in a respectful and loving family who encouraged me to pursue my dreams and ambitions.</p>
     </>
   );
--- src/pages/AboutMe/Interests/index.jsx
+++ src/pages/AboutMe/Interests/index.jsx
@@ -1,11 +1,11 @@
 const Interests = () => {
   return (
     <>
-      <p>👨‍👩‍👧‍👦 Family & Faith - Devoted to family life, faith, and meaningful connections.</p>
-      <p>📚 Continuous Learning - Passionate about personal growth, new skills, and self-improvement.</p>
-      <p>🎸 Music & Instruments - Playing acoustic guitar and keyboards since childhood, always exploring new sounds.</p>
-      <p>🎮 Gaming - From classic video games to modern ones, always up for a good challenge.</p>
-      <p>💪 Sports & Activities - Enjoys staying active, and the gym is my preferred option.</p>
+      <p className="mb-3">👨‍👩‍👧‍👦 Family & Faith - Devoted to family life, faith, and meaningful connections.</p>
+      <p className="mb-3">📚 Continuous Learning - Passionate about personal growth, new skills, and self-improvement.</p>
+      <p className="mb-3">🎸 Music & Instruments - Playing acoustic guitar and keyboards since childhood, always exploring new sounds.</p>
+      <p className="mb-3">🎮 Gaming - From classic video games to modern ones, always up for a good challenge.</p>
+      <p className="mb-3">💪 Sports & Activities - Enjoys staying active, and the gym is my preferred option.</p>
       <p>🎤 Communication & Leadership - Loves sharing ideas, leading small groups, and inspiring others.</p>
     </>
   );
