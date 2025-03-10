2fd82f123db7155444590771207de921d17d6a3e
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 17:45:51 -0300

    soft skills page

--- src/pages/AboutMe/ProfessionalInfo/SoftSkills/index.jsx
+++ src/pages/AboutMe/ProfessionalInfo/SoftSkills/index.jsx
@@ -1,7 +1,15 @@
 const SoftSkills = () => {
   return (
-    <p>SoftSkills</p>
-  );
-};
+    <>
+      <p className="mb-3">// Leadership & Team Collaboration</p>
+      <p className="mb-3">// Organization & Time Management</p>
+      <p className="mb-3">// Problem-Solving & Critical Thinking</p>
+      <p className="mb-3">// Communication & Didactic Skills</p>
+      <p className="mb-3">// Adaptability & Continuous Learning</p>
+      <p className="mb-3">// Initiative & Proactivity</p>
+      <p>// Professionalism & Ethics</p>
+    </>
+  )
+}
 
-export default SoftSkills;
+export default SoftSkills
