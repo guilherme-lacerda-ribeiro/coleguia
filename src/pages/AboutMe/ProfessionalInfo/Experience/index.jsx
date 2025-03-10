55ff816ee78ccd83b3fc5b2a16099f8418ad5349
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 16:50:33 -0300

    experience and hard skills page

--- src/pages/AboutMe/ProfessionalInfo/Experience/index.jsx
+++ src/pages/AboutMe/ProfessionalInfo/Experience/index.jsx
@@ -1,6 +1,13 @@
 const Experience = () => {
   return (
-    <p>Experience</p>
+    <>
+      <p className="mb-3"># Devops</p>
+      <p className="mb-3"># Infrastructure</p>
+      <p className="mb-3"># Database</p>
+      <p className="mb-3"># Tech Lead</p>
+      <p className="mb-3"># Software Development</p>
+      <p>Networking</p>
+    </>
   );
 };
 
--- src/pages/AboutMe/ProfessionalInfo/HardSkills/index.jsx
+++ src/pages/AboutMe/ProfessionalInfo/HardSkills/index.jsx
@@ -1,6 +1,13 @@
 const HardSkills = () => {
   return (
-    <p>HardSkills</p>
+    <>
+      <p className="mb-3"># Python: 2017 - now</p>
+      <p className="mb-3"># Javascript: 2008 - now</p>
+      <p className="mb-3"># PHP: 2007 - now</p>
+      <p className="mb-3"># MySQL: 2007 - now</p>
+      <p className="mb-3"># Java: 2006 - now</p>
+      <p className="mb-3"># Cobol: 2005 - 2007</p>
+    </>
   );
 };
 
