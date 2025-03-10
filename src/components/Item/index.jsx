1dd808024b099ee513e903e80abb50a78c87b60f
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 10:35:49 -0300

    props drilling to animate icons

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,10 +1,14 @@
 import ItemArrow from "../ItemArrow";
 import Folder from "../Folder";
+import { useState } from "react";
 
 const Item = ({ fillColor, children }) => {
+  const [itemOpen, setItemOpen] = useState(false)
+  const handleClickItem = () => setItemOpen(prev => !prev)
+
   return (
-    <li className="flex gap-2">
-      <ItemArrow />
+    <li className="flex gap-2" onClick={handleClickItem}>
+      <ItemArrow itemOpen={itemOpen} />
       <Folder fillColor={fillColor} />
       {children}
     </li>
