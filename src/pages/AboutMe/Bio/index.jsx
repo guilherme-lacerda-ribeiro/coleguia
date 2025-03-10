65f9f3a6b83a4060d9c1bb3af1f74a057bde6cd9
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 15:41:43 -0300

    link colored

--- src/pages/AboutMe/Bio/index.jsx
+++ src/pages/AboutMe/Bio/index.jsx
@@ -1,7 +1,7 @@
 const Bio = () => {
   return (
     <>
-      <p>📌 Born in 1984, in <a href="https://maps.app.goo.gl/VAUYDJj3nDGwifr3A">Belo Horizonte, MG, Brazil</a>.</p>
+      <p>📌 Born in 1984, in <a className="underline text-sky-600" href="https://maps.app.goo.gl/VAUYDJj3nDGwifr3A" target='_blank'>Belo Horizonte, MG, Brazil</a>.</p>
       <p>👶 Spent my childhood as an active child, engaging in sports, video games, and developing strong communication skills.</p>
       <p>👨‍👩‍👧‍👦 I was raised in a respectful and loving family who encouraged me to pursue my dreams and ambitions.</p>
     </>
--- src/pages/AboutMe/PersonalInfo/index.jsx
+++ src/pages/AboutMe/PersonalInfo/index.jsx
@@ -27,13 +27,19 @@ const PersonalInfo = () => {
 
       <div className={`transition-all duration-300 ease-in-out overflow-auto ${sectionOpen ? "max-h-[min(65vh,600px)] opacity-100" : "max-h-0 opacity-0"}`} >
         <ul className="bg-slate-950 p-5 flex flex-col gap-4">
-          <Item className='flex flex-col gap-3' name='bio' fillColor="fill-rose-400" hideContent={activeItem !== 'bio'} handleHideContent={() => toggleActiveItem('bio')}>
+
+          <Item className='flex flex-col gap-3' name='bio' fillColor="fill-rose-400"
+            hideContent={activeItem !== 'bio'} handleHideContent={() => toggleActiveItem('bio')} >
             <Bio />
           </Item>
-          <Item className='flex flex-col gap-3' name='interests' fillColor="fill-teal-400" hideContent={activeItem !== 'interests'} handleHideContent={() => toggleActiveItem('interests')}>
+
+          <Item className='flex flex-col gap-3' name='interests' fillColor="fill-teal-400"
+            hideContent={activeItem !== 'interests'} handleHideContent={() => toggleActiveItem('interests')} >
             <Interests />
           </Item>
-          <Item className='flex flex-col gap-3' name='education' fillColor="fill-indigo-500" hideContent={activeItem !== 'education'} handleHideContent={() => toggleActiveItem('education')}>
+
+          <Item className='flex flex-col gap-3' name='education' fillColor="fill-indigo-500"
+            hideContent={activeItem !== 'education'} handleHideContent={() => toggleActiveItem('education')} >
             <Education />
           </Item>
         </ul>
