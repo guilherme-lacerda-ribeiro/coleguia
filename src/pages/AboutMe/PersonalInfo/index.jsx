82a6b8e59f0d0b44ef466774c8a8c475c1a22352
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 16:05:44 -0300

    item container component refactored from personal info page

--- src/pages/AboutMe/PersonalInfo/index.jsx
+++ src/pages/AboutMe/PersonalInfo/index.jsx
@@ -4,49 +4,32 @@ import Item from "../../../components/Item";
 import Bio from "../Bio";
 import Interests from "../Interests";
 import Education from "../Education";
+import ItemContainer from "../../../components/ItemContainer";
 
 const PersonalInfo = () => {
-  const [sectionOpen, setSectionOpen] = useState(false)
-  const handleSectionClick = () => setSectionOpen(prev => !prev)
-
   const [activeItem, setActiveItem] = useState('')
   const toggleActiveItem = (itemName) => {
-    console.log('click')
     setActiveItem(activeItem === itemName ? '' : itemName)
   }
 
   return (
-    <div id='section-inner' className="flex flex-col gap-3">
+    <ItemContainer title='personal-info'>
+      <Item name='bio' fillColor="fill-rose-400"
+        hideContent={activeItem !== 'bio'} handleHideContent={() => toggleActiveItem('bio')} >
+        <Bio />
+      </Item>
 
-      <h4 className="flex flex-row content-center text-slate-50 px-5" onClick={handleSectionClick}>
-        <button>
-          <SectionArrow sectionOpen={sectionOpen} />
-        </button>
-        personal-info
-      </h4>
+      <Item name='interests' fillColor="fill-teal-400"
+        hideContent={activeItem !== 'interests'} handleHideContent={() => toggleActiveItem('interests')} >
+        <Interests />
+      </Item>
 
-      <div className={`transition-all duration-300 ease-in-out overflow-auto ${sectionOpen ? "max-h-[min(65vh,600px)] opacity-100" : "max-h-0 opacity-0"}`} >
-        <ul className="bg-slate-950 p-5 flex flex-col gap-4">
-
-          <Item name='bio' fillColor="fill-rose-400"
-            hideContent={activeItem !== 'bio'} handleHideContent={() => toggleActiveItem('bio')} >
-            <Bio />
-          </Item>
-
-          <Item name='interests' fillColor="fill-teal-400"
-            hideContent={activeItem !== 'interests'} handleHideContent={() => toggleActiveItem('interests')} >
-            <Interests />
-          </Item>
-
-          <Item name='education' fillColor="fill-indigo-500"
-            hideContent={activeItem !== 'education'} handleHideContent={() => toggleActiveItem('education')} >
-            <Education />
-          </Item>
-        </ul>
-      </div>
-
-    </div>
-  );
-};
+      <Item name='education' fillColor="fill-indigo-500"
+        hideContent={activeItem !== 'education'} handleHideContent={() => toggleActiveItem('education')} >
+        <Education />
+      </Item>
+    </ItemContainer>
+  )
+}
 
 export default PersonalInfo;
