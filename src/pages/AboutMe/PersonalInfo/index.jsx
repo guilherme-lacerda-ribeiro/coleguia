3dcae6a63f155e2f50da68f85b4af0aa43b97331
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-10 10:44:34 -0300

    open close section effect

--- src/pages/AboutMe/PersonalInfo/index.jsx
+++ src/pages/AboutMe/PersonalInfo/index.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import SectionArrow from "../../../components/SectionArrow";
-import Folder from "../../../components/Folder";
-import ItemArrow from "../../../components/ItemArrow";
 import Item from "../../../components/Item";
 
 const PersonalInfo = () => {
@@ -9,26 +7,22 @@ const PersonalInfo = () => {
   const handleSectionClick = () => setSectionOpen(prev => !prev)
 
   return (
-    <div id='section-inner' className="flex flex-col gap-4">
-      
+    <div id='section-inner' className="flex flex-col gap-3">
+
       <h4 className="flex flex-row content-center text-slate-50 px-5" onClick={handleSectionClick}>
         <button>
           <SectionArrow sectionOpen={sectionOpen} />
         </button>
         personal-info
       </h4>
-      
-      <ul className="bg-slate-950 p-5 flex flex-col gap-4">
-        <Item fillColor="fill-rose-400">
-          bio
-        </Item>
-        <Item fillColor="fill-teal-400">
-          interests
-        </Item>
-        <Item fillColor="fill-indigo-500">
-          education
-        </Item>
-      </ul>
+
+      <div className={`transition-all duration-300 ease-in-out overflow-hidden ${sectionOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`} >
+        <ul className="bg-slate-950 p-5 flex flex-col gap-4">
+          <Item fillColor="fill-rose-400">bio</Item>
+          <Item fillColor="fill-teal-400">interests</Item>
+          <Item fillColor="fill-indigo-500">education</Item>
+        </ul>
+      </div>
 
     </div>
   );
