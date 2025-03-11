32604f7b693ae862df38e6c248f9931db7c64e86
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-11 20:15:35 -0300

    projects page finished

--- src/pages/Projects/ProjectFilter/index.jsx
+++ src/pages/Projects/ProjectFilter/index.jsx
@@ -11,15 +11,28 @@ const ProjectFilter = ({ setTechsFilter }) => {
       .catch(console.log)
   }, [])
 
+  const handleFilterChange = (e) => {
+    const checkbox = e.currentTarget.querySelector("input[type=checkbox]")
+    if (!checkbox) return
+    const techId = Number(checkbox.value)
+
+    setTechsFilter(prev =>
+      checkbox.checked
+        ? [...prev, techId] // add if checked
+        : prev.filter(id => id !== techId) // remove if unchecked
+    )
+  }
+
   return (
     <ItemContainer title='project-filter'>
       <ul className="flex flex-col gap-2">
         {
           techs.map(t =>
-            <li>
-
-              <label className="flex items-center gap-2 cursor-pointer">
+            <li key={t.id}>
+              <label className="flex items-center gap-2 cursor-pointer" onClick={handleFilterChange}>
+                
                 <input type="checkbox" id={`tech_${t.id}`} value={t.id} className="peer hidden" />
+                
                 <div className="size-5 border border-gray-400 rounded-sm mr-1
                   flex items-center justify-center 
                 peer-checked:bg-slate-500 peer-checked:border-slate-700
@@ -27,14 +40,14 @@ const ProjectFilter = ({ setTechsFilter }) => {
                 
                 <img src={t.icon} className="size-6" />
                 <span>{t.name}</span>
+              
               </label>
-
             </li>
           )
         }
       </ul>
     </ItemContainer>
-  );
-};
+  )
+}
 
-export default ProjectFilter;
+export default ProjectFilter
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -5,35 +5,50 @@ import ProjectCard from './ProjectCard';
 const Projects = () => {
   const [projects, setProjects] = useState([])
   const [techsFilter, setTechsFilter] = useState([])
+  const [filteredProjects, setFilteredProjects] = useState([])
 
   useEffect(() => {
     fetch('/data/project-catalog.json')
       .then(r => r.json())
-      .then(data => setProjects(data))
+      .then(data => {setProjects(data); setFilteredProjects(data)})
       .catch(console.log)
   }, [])
 
   useEffect(() => {
-    console.log(techsFilter)
-  }, [techsFilter])
+    if (techsFilter.length === 0) { setFilteredProjects(projects); return }
+
+    setFilteredProjects(
+      projects.filter(
+        project => project.techs.some(
+          tech => techsFilter.includes(tech)
+        )
+      )
+    )
+
+  }, [techsFilter, projects])
 
   return (
     <>
       <h2 className="px-7 py-5 text-slate-300">_projects</h2>
       <div id='project-wrapper' className="flex flex-col gap-1">
+        
         <section className="bg-slate-700 pt-3">
           <ProjectFilter setTechsFilter={setTechsFilter} />
         </section>
+
         <section id='project-list'>
           <ul className='pt-3 pb-7 flex flex-col gap-7'>
             {
-              projects.map(p => <li> <ProjectCard project={p}/> </li>)
+              filteredProjects.map(p =>
+                <li key={p.id}> <ProjectCard project={p}/> </li>
+              )
             }
           </ul>
         </section>
+        
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
