925a5ca1b8112281a09a611b64cf80d2d45b719a
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-11 09:47:21 -0300

    removing game in interests

--- src/pages/AboutMe/PersonalInfo/Interests/index.jsx
+++ src/pages/AboutMe/PersonalInfo/Interests/index.jsx
@@ -4,7 +4,6 @@ const Interests = () => {
       <p className="mb-3">👨‍👩‍👧‍👦 Family & Faith - Devoted to family life, faith, and meaningful connections.</p>
       <p className="mb-3">📚 Continuous Learning - Passionate about personal growth, new skills, and self-improvement.</p>
       <p className="mb-3">🎸 Music & Instruments - Playing acoustic guitar and keyboards since childhood, always exploring new sounds.</p>
-      <p className="mb-3">🎮 Gaming - From classic video games to modern ones, always up for a good challenge.</p>
       <p className="mb-3">💪 Sports & Activities - Enjoys staying active, and the gym is my preferred option.</p>
       <p>🎤 Communication & Leadership - Loves sharing ideas, leading small groups, and inspiring others.</p>
     </>
