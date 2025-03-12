528b14a94ea1dea6ea829b2c34aebb8a1b6c4229
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-11 21:11:11 -0300

    hello height fixed

--- src/pages/Hello/index.jsx
+++ src/pages/Hello/index.jsx
@@ -1,6 +1,6 @@
 const Hello = () => {
   return (
-    <div className="size-full bg-[url(/img/hello-bg.png)] bg-no-repeat bg-cover flex flex-col justify-center content-center gap-20 px-8">
+    <div className="min-h-[calc(100vh-9rem)] bg-[url(/img/hello-bg.png)] bg-no-repeat bg-cover flex flex-col justify-center content-center gap-20 px-8">
       <section className="flex flex-col justify-center content-center gap-4">
         <p className="text-xl">
           Hi all. I am
